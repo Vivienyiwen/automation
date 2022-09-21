1b785a3153f7cde4207cd4f681c4b4e983cc7e57
yiwen <97607988+Vivienyiwen@users.noreply.github.com>
2022-09-21 17:29:51 +0800

    Update auto_Whatsapp.py
    I try to figure out the garbled code problem, then I decided to remove the words I don't need.
    And here I import re, and use the findall() function.

--- Whatsapp/auto_Whatsapp.py
+++ Whatsapp/auto_Whatsapp.py
@@ -2,18 +2,23 @@ import pyautogui as pg
 import webbrowser as web
 import time
 import pandas as pd
+import re
 
 #Ensure that the .csv is in the correct format, otherwise the encoding format may be incorrect!
-data = pd.read_csv('/Users/wen9953/Desktop/REV/Auto_Whatsapp/Customer03.csv')
+data = pd.read_csv('/Users/wen9953/Desktop/REV/Auto_Whatsapp/Customer04.csv', encoding='utf-8-sig')
+
 data_dict = data.to_dict('list')
 leads = data_dict['phone'] #Enter the corresponding column name in single quotes, and make sure the column name is correct
 instructors = data_dict['INSTRUCTOR']
 name = data_dict['NAME']
 combo = zip(leads, instructors, name)
 first = True
-for lead, instructor, name in combo:
+for leads, instructors, name in combo:
     time.sleep(4)
-    web.open("https://web.whatsapp.com/send?phone=" + lead + "&text=" + "Hello " + name + "! I'm Chevon, a membership consultant from Revolution and I noticed that you recently attended " + instructor + "'s class! How was it? I would love to hear any feedback that you may have for us! :)")
+    
+    i = instructors
+    i = ''.join(re.findall('[A-Za-z]*\s*[A-Za-z]*\s*[A-Za-z]', i))
+    web.open('https://web.whatsapp.com/send?phone=' + leads + '&text=' + "Hello " + name + "! I'm Chevon, a membership consultant from Revolution and I noticed that you recently attended " + i + "'s class! How was it? I would love to hear any feedback that you may have for us! :)")
     if first:
         time.sleep(6)
         first = False
@@ -23,3 +28,4 @@ for lead, instructor, name in combo:
     pg.press('enter')
     time.sleep(8)
     pg.hotkey('ctrl', 'w')
+
